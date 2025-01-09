0f76f6f8ff40cf24ff30d53a280e53f9cbdfdabc
Andy Topley <andy@andytopley.co.uk>
2025-01-09 12:13:07 +0000

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Andy Topley on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
